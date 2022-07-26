3350f6817dd8a760eab609840b91741b0ea1d564
Mohammadreza Fekri <fekri.mohammadreza86@gmail.com>
2022-07-26 17:04:34 +0430

    init commit

--- app/src/main/java/com/app/fragments/ui/activity/MainActivity.kt
+++ app/src/main/java/com/app/fragments/ui/activity/MainActivity.kt
@@ -2,13 +2,9 @@ package com.app.fragments.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.graphics.translationMatrix
-import androidx.fragment.app.Fragment
 import com.app.fragments.R
 import com.app.fragments.databinding.ActivityMainBinding
 import com.app.fragments.ui.fragment.FragmentFirst
-import com.app.fragments.ui.fragment.FragmentSecond
-import com.app.fragments.ui.fragment.FragmentThird
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -18,10 +14,14 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.add(R.id.frame_main_container, FragmentFirst())
-        transaction.addToBackStack(null)
-        transaction.commit()
+        binding.btnOpenFragmentFirst.setOnClickListener {
+
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.add(R.id.frame_main_container, FragmentFirst())
+            transaction.addToBackStack(null)
+            transaction.commit()
+
+        }
 
     }
 
--- app/src/main/java/com/app/fragments/ui/fragment/FragmentFirst.kt
+++ app/src/main/java/com/app/fragments/ui/fragment/FragmentFirst.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.app.fragments.R
 import com.app.fragments.databinding.FirstFragmentBinding
+import com.app.fragments.ux.Person
 
 class FragmentFirst: Fragment() {
     lateinit var binding: FirstFragmentBinding
@@ -21,12 +23,30 @@ class FragmentFirst: Fragment() {
 
         binding.btnGoToSecondFragment.setOnClickListener {
 
-            // load second fragment in first fragment :
+            val name = binding.edtName.text.toString()
+            val lastname = binding.edtLastName.text.toString()
+            val age = binding.edtAge.text.toString()
+            val gender = true
 
-            val transaction = childFragmentManager.beginTransaction()
-            transaction.add(R.id.frame_first_fragment_container, FragmentSecond())
-            transaction.addToBackStack(null)
-            transaction.commit()
+            if (name.isNotEmpty() && lastname.isNotEmpty()) {
+
+                val data = Person(name, lastname, age, gender)
+
+                val bundle = Bundle()
+                bundle.putParcelable( SEND_PARCELABLE_KEY , data )
+
+
+                val fragment = FragmentSecond()
+                fragment.arguments = bundle
+
+                val transaction = parentFragmentManager.beginTransaction()
+                transaction.replace(R.id.frame_main_container, fragment)
+                transaction.addToBackStack(null)
+                transaction.commit()
+
+            }else {
+                Toast.makeText(context, "Please, enter your name and last name", Toast.LENGTH_LONG).show()
+            }
 
         }
 
